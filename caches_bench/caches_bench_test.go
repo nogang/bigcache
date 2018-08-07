c9c4b11e1eaf44a8d4cf73c7b7b44b93ee1a823b
rick <rick.no@groundx.xyz>
2018-08-07 17:58:03 +0900

    gocache time modify

--- caches_bench/caches_bench_test.go
+++ caches_bench/caches_bench_test.go
@@ -100,7 +100,7 @@ func (gc *goCache)Add(key, value interface{}) (ok bool) {
 	k, ok := key.(string)
 
 	if ok {
-		gc.cache.Add(k,value,1000)
+		gc.cache.Add(k,value, GOCACHE.DefaultExpiration)
 	}
 	return
 }
@@ -286,3 +286,51 @@ func parallelKey(threadID int, counter int) string {
 
 
 /////랜덤 테스트 구현/////
+/*
+func BenchmarkCacheParellalAddTest2(b *testing.B){
+	benchCacheTest2(b, parallelGetTestFunc2)
+}
+
+func benchCacheTest2(b *testing.B, tf TestFunc){
+	benchmarks := []BM{}
+	cacheName := []string{FreeCache}
+
+	for i := 0 ; i < len(cacheName) ; i++ {
+		for cacheSize := 10000000 ; cacheSize <= 10000000 ; cacheSize *= 10 {
+			//for cacheSize := 1000 ; cacheSize <= 1000 ; cacheSize *= 10 {
+			if cacheName[i] == GoCache || cacheName[i] == SyncMap {
+				benchmarks = append(benchmarks,BM{name:cacheName[i],cacheSize:cacheSize,inDataSize:cacheSize})
+			} else {
+				benchmarks = append(benchmarks,BM{name:cacheName[i],cacheSize:cacheSize,inDataSize:cacheSize/10})
+				//benchmarks = append(benchmarks,BM{name:cacheName[i],cacheSize:cacheSize,inDataSize:cacheSize/100})
+				//benchmarks = append(benchmarks,BM{name:cacheName[i],cacheSize:cacheSize,inDataSize:cacheSize/1000})
+			}
+		}
+	}
+
+	var cache Cache
+	for _, bm := range benchmarks {
+		cache, _ = newCache(bm.name, bm.cacheSize)
+
+		for i := 0; i < bm.inDataSize; i++ {
+			cache.Add(key(i), value())
+		}
+
+		tf(b, cache, bm)
+	}
+}
+var parallelGetTestFunc2 = func(b *testing.B, cache Cache, bm BM){
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	b.SetParallelism(maxGoroutine)
+	b.ResetTimer()
+
+		b.RunParallel(func(pb *testing.PB) {
+			counter := 0
+			for pb.Next() {
+				cache.Get(key(counter % bm.inDataSize))
+				counter++
+			}
+		})
+
+}
+*/
